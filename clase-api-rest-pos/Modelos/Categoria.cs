683a688f95983be933f55a4a057ef30f88b692e3
Brandon Cahuec <brandoncahuecc@outlook.com>
2024-08-03 11:03:48 -0600

    Cargando archivos faltantes

--- clase-api-rest-pos/Modelos/Categoria.cs
+++ clase-api-rest-pos/Modelos/Categoria.cs
@@ -3,7 +3,7 @@
 public class Categoria
 {
     public int Id { get; set; }
-    public string Nombre { get; set; }
-    public string Descripcion { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+    public string Descripcion { get; set; } = string.Empty;
     public bool Condicion { get; set; }
 }
